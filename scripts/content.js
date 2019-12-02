6f1368f4957088bf2210b6e6ee413e575195cd6b
tcoppex <tcoppex@gmail.com>
2019-12-02 15:44:47 +0100

    Update getPageAuthor for new meta

--- scripts/content.js
+++ scripts/content.js
@@ -9,19 +9,25 @@ function getPageAuthor() {
   return [].slice.call(document.getElementsByTagName('META'))
     // keep only meta with attributes
     .filter(e => typeof e.attributes !== 'undefined')
-    // defined as 'author' or 'creator'
+    // having property
     .filter(e => { 
-      let value = e.attributes[0].value;
-      return (-1 != value.search('author')) 
-          || (-1 != value.search('creator'))
-          || (-1 != value.search('twitter:creator'))
-          || (-1 != value.search('twitter:site'))
-          || (-1 != value.search('og:site_name'))
+      return e.attributes['itemprop'] 
+          || e.attributes['name'] 
+          || e.attributes['property']
+    })
+    //defined as 'author' or 'creator'
+    .filter( e => {
+      let value = e.name || e.content || '';
+      return value.endsWith('author') 
+          || value.endsWith('creator')
+          || value.endsWith('site_name')
+          || value.endsWith('site')
           ;
     })
     // keep only one, or the hostname if none exists.
-    .reduce((a, curr) => curr.content, window.location.hostname)
-    .split(',')[0];
+    .reduce((a, curr) => curr.attributes[1].value, window.location.hostname)
+    .split(',')[0]
+    ;
 }
 
 /** Send the Selection data to the background extension script */
